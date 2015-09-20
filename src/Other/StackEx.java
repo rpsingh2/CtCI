de9c2083272a3558396e3ce95a85349d8a9911b9
Shubham <shubham.bhawsinka@gmail.com>
2015-09-19 23:38:28 -0400

    Adding more String solutions

---
+++ src/Other/StackEx.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Other;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ *
+ * @author Shubham
+ */
+public class StackEx {
+    public static void main(String args[]){
+        System.out.println("Enter String to check");
+        Scanner s1= new Scanner(System.in);
+        Stack s = new Stack(); 
+        String input = s1.nextLine();
+        int length=input.length();
+        for(int i=0;i<length;i++){
+            if(s.isEmpty())
+                s.push(input.charAt(i));
+            else{
+            char c = input.charAt(i);
+            char current=(char)s.peek();
+            
+            if(current==c){
+                s.pop();
+            }
+            else{
+                s.push(c);
+            }
+        }
+        }
+        if(s.isEmpty())
+            System.out.println("The string is right");
+        else{
+            System.out.println("The string is wrong");
+        }
+        
+
+    }
+}
---
+++ src/Strings/RemoveSpaceFromString.java
@@ -0,0 +1,37 @@
+/*
+Code to remove space from a sentence
+ */
+package Strings;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Shubham
+ */
+public class RemoveSpaceFromString {
+    public static void main(String args[]){
+        System.out.println("Enter the string to be trimmed-");
+        Scanner s=new Scanner(System.in);
+        String input=s.nextLine();
+        int j=0;
+        StringBuilder st= new StringBuilder();
+        for(int i=0; i<input.length();i++){
+            char c=input.charAt(i);
+            if(c==' ' && st.length()!=0){
+                if(st.charAt(j-1)==' ')
+                continue;
+                else{
+                    st.append(c);
+                    j++;
+                }
+            }
+            else{
+                st.append(c);
+                j++;
+            }
+
+        }
+        System.out.println(st.toString());
+    }
+}
