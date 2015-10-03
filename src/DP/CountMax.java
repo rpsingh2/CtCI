36d565898e578fd7aaac9661d83820c4b59c161c
Shubham <shubham.bhawsinka@gmail.com>
2015-10-03 19:44:13 -0400

    Matrix Multiplication

--- src/DP/CountMax.java
+++ src/DP/CountMax.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Given two integers ‘n’ and ‘sum’, find count of all n digit numbers with sum of digits as ‘sum’. Leading 0’s are not counted as digits.
+1 <= n <= 100 and 1 <= sum <= 50000
  */
 package DP;
 
@@ -11,4 +10,14 @@ package DP;
  */
 public class CountMax {
     
+    public static int cmax(int n, int sum){
+        
+        return 0;
+    }
+    public static void main(String args[]){
+        int n=2;
+        int sum=5;
+        int output = cmax(n,sum);
+        System.out.println("Maximum possible numbers are: " + output);
+    }
 }
---
+++ src/DP/MatrixMulEff.java
@@ -0,0 +1,47 @@
+/*
+Given a sequence of matrices, find the most efficient way to multiply these matrices together. 
+The problem is not actually to perform the multiplications, but merely to decide in which order to perform the multiplications.
+ */
+package DP;
+
+/**
+ *
+ * @author Shubham
+ */
+public class MatrixMulEff {
+    
+    
+    public static void efficient_mul(int[] arr, int n){
+    int[][] m = new int[n][n];
+ 
+    int i, j, k, L, q;
+ 
+    // cost is zero when multiplying one matrix.
+    for (i = 0; i < n; i++)
+        m[i][i] = 0;
+ 
+    // L is chain length.  
+    for (L=2; L<n; L++)   
+    {
+        for (i=1; i<n-L+1; i++)
+        {
+            j = i+L-1;
+            m[i][j] = 999999;
+            for (k=i; k<j; k++)
+            {
+                // q = cost/scalar multiplications
+                q = m[i][k] + m[k+1][j] + arr[i-1]*arr[k]*arr[j];
+                if (q < m[i][j])
+                    m[i][j] = q;
+            }
+        }
+    }
+ 
+    System.out.print(m[1][n-1]);
+}
+    
+    public static void main(String args[]){
+        int[] arr= {10,20,30,40};
+        efficient_mul(arr, arr.length);
+    }
+}
