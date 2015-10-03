ee94c585beede463ecbf9991083bb17460a48a89
Shubham <shubham.bhawsinka@gmail.com>
2015-10-03 16:50:29 -0400

    Number of steps DP

---
+++ src/DP/NumberOfSteps.java
@@ -0,0 +1,32 @@
+/*
+*Given a distance ‘dist, count total number of ways to cover the distance with 1, 2 and 3 steps.
+ */
+package DP;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Shubham
+ */
+public class NumberOfSteps {
+    public static void stepnum(int input){
+        if(input <= 0)
+            return;
+        int[] arr = new int[input];
+        arr[0]=1;arr[1]=1;arr[2]=2;
+        for(int i=3;i<input;i++){
+            arr[i]=arr[i-1] + arr[i-2] + arr[i-3];
+        }
+        System.out.println(arr[input-1]);
+        
+    }
+    
+    
+    public static void main(String args[]){
+        System.out.println("Enter the total distance");
+        Scanner s=new Scanner(System.in);
+        int input=s.nextInt();
+        stepnum(input);
+    }
+}
--- src/Other/RotatedSort.java
+++ src/Other/RotatedSort.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Search element in a circularly sorted array
  */
 package Other;
 
